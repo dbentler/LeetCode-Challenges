33a13a78beb50f59fb2a2fc3498776b69508bca0
dbentler <db3857@nyu.edu>
2022-02-10 10:01:44 -0500

    Solved #66: Plus One (Python, casting abuse)

---
+++ python/66_plus_one.py
@@ -0,0 +1,39 @@
+"""
+You are given a large integer represented as an integer array digits, where each digits[i] is the ith digit of the integer. The digits are ordered from most significant to least significant in left-to-right order. The large integer does not contain any leading 0's.
+
+Increment the large integer by one and return the resulting array of digits.
+
+Example 1:
+
+Input: digits = [1,2,3]
+Output: [1,2,4]
+Explanation: The array represents the integer 123.
+Incrementing by one gives 123 + 1 = 124.
+Thus, the result should be [1,2,4].
+
+Example 2:
+Input: digits = [4,3,2,1]
+Output: [4,3,2,2]
+Explanation: The array represents the integer 4321.
+Incrementing by one gives 4321 + 1 = 4322.
+Thus, the result should be [4,3,2,2].
+
+Example 3:
+Input: digits = [9]
+Output: [1,0]
+Explanation: The array represents the integer 9.
+Incrementing by one gives 9 + 1 = 10.
+Thus, the result should be [1,0].
+"""
+
+def plusOne(digits):
+    string_num = ""
+    for value in digits:
+        string_num += str(value)
+    value_plus_one = str(int(string_num) + 1)
+    return [int(value) for value in value_plus_one]
+
+if __name__ == "__main__":
+    number = [1, 2, 3]
+    number_p1 = plusOne(number)
+    print(number_p1)
