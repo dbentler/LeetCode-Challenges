afbefdfdecb48440e2746a156fe65c81d173286a
dbentler <db3857@nyu.edu>
2022-05-19 22:33:39 -0400

    Removed unused import I didn't even notice

--- python/2_add_two_num.py
+++ python/2_add_two_num.py
@@ -4,9 +4,6 @@ You are given two non-empty linked lists representing two non-negative integers.
 You may assume the two numbers do not contain any leading zero, except the number 0 itself.
 """
 
-from ast import Num
-
-
 class ListNode:
     def __init__(self, val=0, next=None):
         self.val = val
